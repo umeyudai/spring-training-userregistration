cc34e8b0093e45604ce2e40cb071ca6fefff3e6f
umeyudai <ume.yudai@gmail.com>
2021-10-27 21:40:29 +0900

    部署テーブル結合

---
+++ src/main/java/com/umeyudai/springbootuserregistration/domain/user/model/Department.java
@@ -0,0 +1,8 @@
+package com.umeyudai.springbootuserregistration.domain.user.model;
+import lombok.Data;
+
+@Data
+public class Department {
+    private Integer departmentId;
+    private String departmentName;
+}
