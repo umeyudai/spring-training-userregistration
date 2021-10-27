d5b30fc5f8692d19c5ab73c2791b8acf46c06f62
umeyudai <ume.yudai@gmail.com>
2021-10-27 18:36:56 +0900

    ユーザーマスタ全件表示

--- src/main/java/com/umeyudai/springbootuserregistration/controller/UserListController.java
+++ src/main/java/com/umeyudai/springbootuserregistration/controller/UserListController.java
@@ -1,15 +1,27 @@
 package com.umeyudai.springbootuserregistration.controller;
 
+import com.umeyudai.springbootuserregistration.domain.user.model.MUser;
+import com.umeyudai.springbootuserregistration.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserListController{
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/list")
-    public String getUserList() {
+    public String getUserList(Model model) {
+        List<MUser> userList = userService.getUsers();
+        model.addAttribute("userList",userList);
+
         return "user/list";
     }
 }
