b5d26c05b4c000a4ebe98fdfba70caa7946d9de3
umeyudai <ume.yudai@gmail.com>
2021-10-27 10:42:58 +0900

    inital commit

---
+++ src/main/java/com/umeyudai/springbootuserregistration/application/service/UserApplicationService.java
@@ -0,0 +1,18 @@
+package com.umeyudai.springbootuserregistration.application.service;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+
+@Service
+public class UserApplicationService {
+    public Map<String, Integer> getGenderMap() {
+        Map<String, Integer> genderMap = new LinkedHashMap<>();
+        genderMap.put("男性", 1);
+        genderMap.put("女性", 2);
+        return genderMap;
+    }
+}
---
+++ src/main/java/com/umeyudai/springbootuserregistration/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.umeyudai.springbootuserregistration.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login/login";
+    }
+}
