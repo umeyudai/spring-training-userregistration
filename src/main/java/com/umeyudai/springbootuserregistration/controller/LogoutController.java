a541731f400a4ba00fc2e17529d45ebcee09ff5f
umeyudai <ume.yudai@gmail.com>
2021-10-27 16:45:23 +0900

    画面レイアウト

---
+++ src/main/java/com/umeyudai/springbootuserregistration/controller/LogoutController.java
@@ -0,0 +1,16 @@
+package com.umeyudai.springbootuserregistration.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class LogoutController {
+    @PostMapping("/logout")
+    public String postLogout() {
+        log.info("ログアウト");
+        return "redirect:/login";
+    }
+}
+
---
+++ src/main/java/com/umeyudai/springbootuserregistration/controller/UserListController.java
@@ -0,0 +1,15 @@
+package com.umeyudai.springbootuserregistration.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserListController{
+
+    @GetMapping("/list")
+    public String getUserList() {
+        return "user/list";
+    }
+}
