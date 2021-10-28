b0fa20aeb316876c7262ba94bd26ef3ded563d36
umeyudai <ume.yudai@gmail.com>
2021-10-28 14:18:43 +0900

    認可設定

--- src/main/java/com/umeyudai/springbootuserregistration/config/SecurityConfig.java
+++ src/main/java/com/umeyudai/springbootuserregistration/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/user/signup").permitAll()
+                .antMatchers("/admin").hasAnyAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated();
 
         http
